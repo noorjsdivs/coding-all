54dc54df34987373f835f3f40eb3086e416fc34a
noorjsdivs <noor.jsdivs@gmail.com>
2025-12-29 18:06:50 +0600

    qwik added

---
+++ qwik-test/src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Welcome to Qwik</h1>
+      <p>
+        This is a basic Qwik application. Edit this file at{" "}
+        <code>src/routes/index.tsx</code> to get started.
+      </p>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
